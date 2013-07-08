314a460c15a51121d2f45142dbcecaedf48f5dae
James Daniels <james@marginleft.com>
2013-07-07 21:37:52 -0400

    Translating the bundle install errors

--- app/models/bundle.rb
+++ app/models/bundle.rb
@@ -15,17 +15,24 @@ class Bundle < ActiveRecord::Base
     #   errors << "running iOS #{minimum_os_version} or higher" if minimum_os_version
     # end
     missing_capabilities   = capabilities.select{|k,v| v }.keys - Array(device.model.capabilities)
+    missing_capabilities.map! {|c| I18n.t("bundle.capabilities.#{c}")}
     forbidden_capabilities = Array(device.model.capabilities) & capabilities.reject{|k,v| v }.keys
-    errors << "requires #{missing_capabilities.join(', ')} capabilities"  if missing_capabilities.any?
-    errors << "forbids #{forbidden_capabilities.join(', ')} capabilities" if forbidden_capabilities.any?
-    errors << 'requires an iPad' if ipad_only? && device.model.family != :iPad
-		errors << 'is not provisioned' unless enterprise? || devices.include?(device)
-    errors << "requires iOS #{minimum_operating_system}" if device.operating_system < minimum_operating_system
+    forbidden_capabilities.map! {|c| I18n.t("bundle.capabilities.#{c}")}
+    errors << I18n.t('bundle.errors.armv7_thin_minimum_os_issue') if armv7_thin_minimum_os_issue?
+    errors << I18n.t('bundle.errors.required_capabilities', missing_capabilities: missing_capabilities.join(', ')) if missing_capabilities.any?
+    errors << I18n.t('bundle.errors.forbidden_capabilities', forbidden_capabilities: forbidden_capabilities.join(', ')) if forbidden_capabilities.any?
+    errors << I18n.t('bundle.errors.ipad_only') if ipad_only? && device.model.family != :iPad
+		errors << I18n.t('bundle.errors.not_provisioned') unless enterprise? || devices.include?(device)
+    errors << I18n.t('bundle.errors.minimum_os_not_met', minimum_operating_system: minimum_operating_system) if device.operating_system < minimum_operating_system
     errors
   end
 
   def minimum_operating_system
     @minimum_operating_system ||= Ios::Version.new(nil, minimum_os_version)
+  end
+
+  def armv7_thin_minimum_os_issue?
+    armv7_thin? && minimum_os_version.to_i < 4
   end
 
   def expired?
